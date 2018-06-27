aec197559438d63ee88eb34b5e3b7486fb46f549
aliciapflaumer <alpfla7@gmail.com>
2018-06-27 13:51:58 -0400

    Adding initial files
    - Edited the app.js file to display "Hello World" within the app
    
    - Removed starter text from README

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <Text>React Native is pretty cool!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# Exploring React Native
+
+• To start app in development mode, run `npm start`
+
+**Screenshot of app shown on IOS device**
+
+![screenshot of React Native app](assets/images/reactNativeAppScreenshot.jpg)
