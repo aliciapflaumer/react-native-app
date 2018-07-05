fbe7c9f0c42053198d50d6242e58b95b3c7f1c9c
aliciapflaumer <alpfla7@gmail.com>
2018-07-05 17:30:15 -0400

    Added `react-native-navigation`
    Used `npm install react-native-navigation`

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: ''};
+  }
+
   render() {
     return (
       <View style={styles.container}>
